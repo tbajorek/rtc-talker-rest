6342274364f9a74ba5001c2635879f5a9e94becd
tbajorek <tbajorek3@gmail.com>
2018-12-15 16:58:04 +0100

    Moving env mechanism to be able to used by Doctrine commands

--- cli-config.php
+++ cli-config.php
@@ -1,10 +1,16 @@
 <?php
 
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\EntityManager;
+// cli-config.php
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Slim\Container;
+
+/** @var Container $container */
 $container = require_once __DIR__ . '/bootstrap.php';
 
 ConsoleRunner::run(
     ConsoleRunner::createHelperSet($container[EntityManager::class])
 );
+
+
--- src/settings.php
+++ src/settings.php
@@ -1,4 +1,16 @@
 <?php
+
+// Load environment variables
+try {
+    $dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
+    $dotenv->load();
+    $dotenv->required('DEV_MODE')->isBoolean();
+    $dotenv->required('DB_PORT')->isInteger();
+    $dotenv->required(['DB_DRIVER', 'DB_HOST', 'DB_NAME', 'DB_USERNAME', 'DB_PASSWORD', 'DB_CHARSET']);
+} catch (\Dotenv\Exception\ValidationException $e) {
+    die($e->getMessage());
+} catch (\Dotenv\Exception\InvalidPathException $e) {}
+
 return [
     'settings' => [
         'displayErrorDetails' => getenv('DEV_MODE'), // set to false in production
